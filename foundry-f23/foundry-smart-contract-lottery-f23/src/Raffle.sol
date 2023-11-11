01dce521a821700566381cbf8970a4ceb52c2a8e
Bogomil Simitchiev <simitchiev365@abv.bg>
2023-11-11 21:16:58 +0200

    Added event EnteredRaffle and created s_players

--- foundry-f23/foundry-smart-contract-lottery-f23/src/Raffle.sol
+++ foundry-f23/foundry-smart-contract-lottery-f23/src/Raffle.sol
@@ -10,6 +10,10 @@ pragma solidity ^0.8.19;
 contract Raffle {
     error Raffle_NotEnoughEthSent();
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /* Events */
+    event EnteredRaffle(address indexed player);
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
@@ -19,6 +23,8 @@ contract Raffle {
         if (msg.value < i_entranceFee) {
             revert Raffle_NotEnoughEthSent();
         }
+        s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender);
     }
 
     function pickWinner() public {}
