04b310adb29bdfe46534d0d15f83c471e5027c34
Bogomil-Simitchiev <simitchiev365@abv.bg>
2023-12-01 20:36:55 +0200

    Added one more test and created Interactions.sol

---
+++ foundry-f23/foundry-nft-f23/script/Interactions.s.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+
+contract MintBasicNft is Script {
+    string public constant PUG_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+    uint256 deployerKey;
+
+    function run() external {
+        address mostRecentlyDeployedBasicNft = DevOpsTools
+            .get_most_recent_deployment("BasicNft", block.chainid);
+        mintNftOnContract(mostRecentlyDeployedBasicNft);
+    }
+
+    function mintNftOnContract(address basicNftAddress) public {
+        vm.startBroadcast();
+        BasicNft(basicNftAddress).mintNft(PUG_URI);
+        vm.stopBroadcast();
+    }
+}
+
+//data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgMjAwIDIwMCIgd2lkdGg9IjQwMCIgIGhlaWdodD0iNDAwIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPg0KICA8Y2lyY2xlIGN4PSIxMDAiIGN5PSIxMDAiIGZpbGw9InllbGxvdyIgcj0iNzgiIHN0cm9rZT0iYmxhY2siIHN0cm9rZS13aWR0aD0iMyIvPg0KICA8ZyBjbGFzcz0iZXllcyI+DQogICAgPGNpcmNsZSBjeD0iNjEiIGN5PSI4MiIgcj0iMTIiLz4NCiAgICA8Y2lyY2xlIGN4PSIxMjciIGN5PSI4MiIgcj0iMTIiLz4NCiAgPC9nPg0KICA8cGF0aCBkPSJtMTM2LjgxIDExNi41M2MuNjkgMjYuMTctNjQuMTEgNDItODEuNTItLjczIiBzdHlsZT0iZmlsbDpub25lOyBzdHJva2U6IGJsYWNrOyBzdHJva2Utd2lkdGg6IDM7Ii8+DQo8L3N2Zz4
--- foundry-f23/foundry-nft-f23/test/BasicNftTest.t.sol
+++ foundry-f23/foundry-nft-f23/test/BasicNftTest.t.sol
@@ -9,6 +9,9 @@ import {Test} from "forge-std/Test.sol";
 contract BasicNftTest is Test {
     BasicNft public basicNft;
     DeployBasicNft public deployer;
+    string public constant PUG_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+    address public USER = makeAddr("user");
 
     function setUp() public {
         deployer = new DeployBasicNft();
@@ -23,5 +26,12 @@ contract BasicNftTest is Test {
             keccak256(abi.encodePacked(expectedName)) ==
                 keccak256(abi.encodePacked(actualName))
         );
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(PUG_URI);
+
+        assert(basicNft.balanceOf(USER) == 1);
     }
 }
