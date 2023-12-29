17d7b0e1742177d9748502d2093f561644daeb39
Bogomil-Simitchiev <simitchiev365@abv.bg>
2023-12-29 19:49:14 +0200

    Wrote some tests and finished the deploy script

--- foundry-f23/foundry-defi-stablecoin-f23/src/DSCEngine.sol
+++ foundry-f23/foundry-defi-stablecoin-f23/src/DSCEngine.sol
@@ -124,7 +124,22 @@ contract DSCEngine is ReentrancyGuard {
     // External Functions
     ///////////////////
 
-    function depositCollateralAndMintDsc() external {}
+
+    /*
+    * @param tokenCollateralAddress The address of the token to deposit as collateral
+    * @param amountCollateral The amount of collateral to deposit
+    * @param amountDscToMint The amount of decentralized stablecoin to mint
+    * @notice this function will deposit your collateral and mint DSC in one transaction
+
+    */
+    function depositCollateralAndMintDsc(
+        address tokenCollateralAddress,
+        uint256 amountCollateral,
+        uint256 amountDscToMint
+    ) external {
+        depositCollateral(tokenCollateralAddress, amountCollateral);
+        mintDsc(amountDscToMint);
+    }
 
     /*
      * @param follows CEI
@@ -136,7 +151,7 @@ contract DSCEngine is ReentrancyGuard {
         address tokenCollateralAddress,
         uint256 amountCollateral
     )
-        external
+        public
         moreThanZero(amountCollateral)
         isAllowedToken(tokenCollateralAddress)
         nonReentrant
@@ -165,7 +180,7 @@ contract DSCEngine is ReentrancyGuard {
 
     function mintDsc(
         uint256 amountDscToMint
-    ) external moreThanZero(amountDscToMint) nonReentrant {
+    ) public moreThanZero(amountDscToMint) nonReentrant {
         s_DSCMinted[msg.sender] += amountDscToMint;
         revertIfHealthFactorIsBroken(msg.sender);
 
