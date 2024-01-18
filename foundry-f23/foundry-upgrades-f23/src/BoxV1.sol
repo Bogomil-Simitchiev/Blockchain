25cfd342042854d990658b5562ae3060c98db0f0
Bogomil-Simitchiev <simitchiev365@abv.bg>
2024-01-18 22:18:38 +0200

    Finished project and added 2 tests

--- foundry-f23/foundry-upgrades-f23/src/BoxV1.sol
+++ foundry-f23/foundry-upgrades-f23/src/BoxV1.sol
@@ -15,7 +15,7 @@ contract BoxV1 is  Initializable, UUPSUpgradeable, OwnableUpgradeable {
     }
 
     function initialize() public initializer {
-        __Ownable_init();
+        __Ownable_init(msg.sender);
         __UUPSUpgradeable_init();
     }
 
