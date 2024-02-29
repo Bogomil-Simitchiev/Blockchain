f2a4e097822f6c2889ac3f4247467677c3897434
Bogomil-Simitchiev <simitchiev365@abv.bg>
2024-02-29 20:40:56 +0200

    set options to hardhat config file

--- softuni-blockchain/hardhat/hardhat.config.js
+++ softuni-blockchain/hardhat/hardhat.config.js
@@ -2,5 +2,12 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+   defaultNetwork: "hardhat",
+   networks: {
+    hardhat: {},
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/<key>",
+    },
+   },
   solidity: "0.8.24",
 };
