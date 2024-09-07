ba7cff7020e321ef7f9e28af488f745041109332
Bogomil-Simitchiev <simitchiev365@abv.bg>
2024-09-07 14:04:53 +0300

    Updated ERC721 contract

--- softuni-blockchain/ERC721 simple implementation/ERC721.sol
+++ softuni-blockchain/ERC721 simple implementation/ERC721.sol
@@ -8,11 +8,23 @@ contract ERC721 {
         address indexed _to,
         uint256 indexed _tokenId
     );
-  
-    // Token name
+
+    event Approval(
+        address indexed _owner,
+        address indexed _approved,
+        uint256 indexed _tokenId
+    );
+
+    event ApprovalForAll(
+        address indexed _owner,
+        address indexed _operator,
+        bool _approved
+    );
+
+    // NFT name
     string private _name;
 
-    // Token symbol
+    // NFT symbol
     string private _symbol;
 
     mapping(uint256 => address) private _owners;
@@ -26,57 +38,52 @@ contract ERC721 {
         _symbol = symbol_;
     }
 
-    function getName() public view returns (string memory) {
+    function getName() external view returns (string memory) {
         return _name;
     }
 
-    function getSymbol() public view returns (string memory) {
+    function getSymbol() external view returns (string memory) {
         return _symbol;
     }
 
     function ownerOf(uint256 _tokenId) public view returns (address) {
+        address ownerOfToken = _owners[_tokenId];
+        require(
+            ownerOfToken != address(0),
+            "Owner of token cannot be zero address!"
+        );
         return _owners[_tokenId];
     }
 
-    function balanceOf(address _owner) public view returns (uint256) {
+    function balanceOf(address _owner) external view returns (uint256) {
+        require(_owner != address(0), "Owner cannot be zero address!");
         return _balances[_owner];
     }
 
-    function mint(address _user, uint256 _tokenId) public payable {
-        require(msg.value > 0, "Cannot set zero or less!");
-        require(_user != address(0), "User cannot be zero address!");
-
-        _balances[_user] = msg.value;
-        _owners[_tokenId] = _user;
-    }
-
-    function transferFrom(
+    function safeTransferFrom(
         address _from,
         address _to,
         uint256 _tokenId
-    ) public payable {
+    ) external payable {
         address owner = ownerOf(_tokenId);
 
         require(owner == _from, "TransferFrom: caller is not the owner");
         require(_to != address(0), "TransferFrom: invalid recipient address");
-        require(msg.sender == owner, "TransferFrom: only owner can change user");
-
-         require(msg.value > 0, "Cannot set zero or less!");
-    
-        require(msg.value <= _balances[_from] , "Invalid amount of money!");
-
-        _balances[_from] -= msg.value;
-        _balances[_to] += msg.value;
-        _owners[_tokenId] = _to;
+        require(
+            msg.sender == owner,
+            "TransferFrom: only owner can change user"
+        );
+        require(msg.value > 0, "Cannot set zero or less!");
         
+        delete _tokenApprovals[_tokenId];
+
+        unchecked {
+            _balances[_from] -= msg.value;
+            _balances[_to] += msg.value;
+        }
+
+        _owners[_tokenId] = _to;
+
         emit Transfer(_from, _to, _tokenId);
-    }
-    function burn(address _owner, uint256 _tokenId) public {
-         address owner = ownerOf(_tokenId);
-         require(owner == _owner, "TransferFrom: caller is not the owner");
-
-         delete _owners[_tokenId];
-         _balances[_owner] = 0;
-
     }
 }
