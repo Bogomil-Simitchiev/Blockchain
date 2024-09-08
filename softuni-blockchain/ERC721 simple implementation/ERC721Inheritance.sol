9d53f91b68a4a5d39023d88467a35c608493d91c
Bogomil-Simitchiev <simitchiev365@abv.bg>
2024-09-08 17:24:11 +0300

    Deployed my Dogs collection NFTs on OpenSea and minted my first NFT

--- softuni-blockchain/ERC721 simple implementation/ERC721Inheritance.sol
+++ softuni-blockchain/ERC721 simple implementation/ERC721Inheritance.sol
@@ -2,8 +2,14 @@
 
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC721 {
-    constructor() ERC721("MyTokenBogi", "Simi") { }
+contract MyNFT is ERC721URIStorage, Ownable {
+    constructor() ERC721("Dogs", "SIMI") Ownable(msg.sender) { }
+
+    function mint(address _to, uint256 _tokenId, string calldata _uri) external onlyOwner {
+       _mint(_to, _tokenId);
+       _setTokenURI(_tokenId, _uri);
+    }
 }
